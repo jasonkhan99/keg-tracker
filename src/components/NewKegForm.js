767eb9973f8130acd04c9660dacdbc407668d690
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-11 13:54:17 -0700

    update newkegform to use reusable form

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { v4 } from 'uuid';
 import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
 
 function NewKegForm(props){
 
@@ -11,29 +12,9 @@ function NewKegForm(props){
 
   return (
     <React.Fragment>
-      <form onSubmit={handleNewKegFormSubmission}>
-        <input
-          type='text'
-          name='brand'
-          placeholder='Brand/Brewery'
-        />
-        <input
-          type='text'
-          name='name'
-          placeholder='Name' 
-        />
-        <input
-          type='text'
-          name='price'
-          placeholder='Price' 
-        />
-        <input
-          type='text'
-          name='alcoholContent'
-          placeholder='ABV' 
-        />
-        <button type='submit'>Add Keg</button>
-      </form>
+      <ReusableForm 
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Add Keg" />
     </React.Fragment>
   );
 }
