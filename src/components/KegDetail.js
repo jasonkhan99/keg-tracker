289452560f2255fe97e8f4e7124850d0c711386e
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-11 11:04:29 -0700

    pass props to keg detail

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,12 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function KegDetail(props){
+  const { keg } = props;
+
   return (
     <React.Fragment>
       <h1>Keg Detail</h1>
+      <h2>{keg.brand}</h2>
+      <h3>{keg.names}</h3>
+      <h4>{keg.price}</h4>
+      <h4>{keg.alcoholContent}</h4>
       <hr/>
     </React.Fragment>
   );
 }
+
+KegDetail.propTypes = {
+  keg: PropTypes.object
+};
 
 export default KegDetail;
