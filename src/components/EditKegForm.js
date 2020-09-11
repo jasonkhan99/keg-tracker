6a3c3000cf195d0000864a5c39bd51244d236261
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-11 14:50:21 -0700

    create edit keg form

---
+++ src/components/EditKegForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+
+function EditKegForm (props) {
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        buttonText="Edit Keg" />
+    </React.Fragment>
+  );
+}
+
+export default EditKegForm;
