f65057aa363c33bf2b3308d11fb3c0efa138d976
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-11 10:37:16 -0700

    pass handle new keg to form as prop

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { v4 } from 'uuid';
+import PropTypes from "prop-types";
 
 function NewKegForm(props){
 
   function handleNewKegFormSubmission(event) {
     event.preventDefault();
-    console.log(event.target.brand.value);
-    console.log(event.target.name.value);
-    console.log(event.target.price.value);
-    console.log(event.target.alcoholContent.value);
+    props.onNewTicketCreation({brand: event.target.brand.value, name: event.target.name.value, price: event.target.price.value, alcoholContent: event.target.alcoholContent.value, id: v4()});
   }
 
   return (
@@ -39,5 +37,9 @@ function NewKegForm(props){
     </React.Fragment>
   );
 }
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
 
 export default NewKegForm;
