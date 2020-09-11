bd0cf9d37d13399d4caef1af21626c7aa0811388
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-11 10:15:24 -0700

    add a form to new keg form

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { v4 } from 'uuid';
 
 function NewKegForm(props){
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.brand.value);
+    console.log(event.target.name.value);
+    console.log(event.target.price.value);
+    console.log(event.target.alcoholContent.value);
+  }
+
   return (
     <React.Fragment>
-      <h3>Lorem Ipsum</h3>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type='text'
+          name='brand'
+          placeholder='Brand/Brewery'
+        />
+        <input
+          type='text'
+          name='name'
+          placeholder='Name' 
+        />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price' 
+        />
+        <input
+          type='text'
+          name='alcoholContent'
+          placeholder='ABV' 
+        />
+        <button type='submit'>Add Keg</button>
+      </form>
     </React.Fragment>
   );
 }
